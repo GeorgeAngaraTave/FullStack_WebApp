9cc2e21f45df1ab1fe5f5c14e63c32bb28e22fed
George <jorgeluis_163@hotmail.com>
2018-08-21 21:29:09 -0500

    Postgres con Rails

---
+++ proyectreils/app/controllers/images_controller.rb
@@ -0,0 +1,12 @@
+class ImagesController < ApplicationController
+    
+    # GET /images
+    # GET /images.json
+    def new
+    end
+    
+    # GET /images
+    # GET /images.json
+    def create
+    end    
+end
